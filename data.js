986f455e00abc3bc8bc9f9af81b6e32ba07f81ad
DGRITS <grizay@icloud.com>
2024-03-18 19:53:30 +0300

    1, 2 task

--- data.js
+++ data.js
@@ -1,23 +1,70 @@
 const dataInfo = `
-{
-    "id": 558,
-    "name": "Elon Tusk",
-    "status": "Alive",
-    "species": "Human",
-    "type": "Human with tusks",
-    "gender": "Male",
-    "origin": {
-        "name": "Earth (Tusk Dimension)",
-        "url": "https://rickandmortyapi.com/api/location/92"
+[
+    {
+        "id": 558,
+        "name": "Elon Tusk",
+        "status": "Alive",
+        "species": "Human",
+        "type": "Human with tusks",
+        "gender": "Male",
+        "origin": {
+            "name": "Earth (Tusk Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/92"
+        },
+        "location": {
+            "name": "Heistotron Base",
+            "url": "https://rickandmortyapi.com/api/location/89"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/558.jpeg",
+        "episode": [
+            "https://rickandmortyapi.com/api/episode/34"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/558",
+        "created": "2020-05-07T10:26:02.535Z"
     },
-    "location": {
-        "name": "Heistotron Base",
-        "url": "https://rickandmortyapi.com/api/location/89"
+    {
+        "id": 781,
+        "name": "Rick's Garage",
+        "status": "Alive",
+        "species": "Robot",
+        "type": "Artificial Intelligence",
+        "gender": "Female",
+        "origin": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/781.jpeg",
+        "episode": [
+            "https://rickandmortyapi.com/api/episode/49"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/781",
+        "created": "2021-10-25T09:18:48.188Z"
     },
-    "image": "https://rickandmortyapi.com/api/character/avatar/558.jpeg",
-    "episode": [
-        "https://rickandmortyapi.com/api/episode/34"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/558",
-    "created": "2020-05-07T10:26:02.535Z"
-}`;
+    {
+        "id": 770,
+        "name": "Hothead Rick",
+        "status": "Dead",
+        "species": "Human",
+        "type": "Rick",
+        "gender": "Male",
+        "origin": {
+            "name": "unknown",
+            "url": ""
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/770.jpeg",
+        "episode": [
+            "https://rickandmortyapi.com/api/episode/48"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/770",
+        "created": "2021-10-25T08:55:48.771Z"
+    }
+]
+`;
